b42ebcfee17d180fbff429c4b70d9e5c09529c75
plaxless <kevin3467@naver.com>
2025-05-31 22:38:41 +0900

    .

--- app/src/main/java/com/example/allaccountbook/uiComponent/ShowDailyScreen.kt
+++ app/src/main/java/com/example/allaccountbook/uiComponent/ShowDailyScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.allaccountbook.uiPersistent.BottomNevBar
 import com.example.allaccountbook.uiPersistent.formatWithCommas
 import com.example.allaccountbook.uiPersistent.showDate
 import java.text.SimpleDateFormat
