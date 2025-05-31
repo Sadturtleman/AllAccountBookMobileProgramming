8b898b0701f9a645f3e0bf38493f4fb7007b05e9
N-ezio <131796234+N-ezio@users.noreply.github.com>
2025-06-01 04:50:24 +0900

    Add files via upload
    캘린더 구성 요소

---
+++ app/src/main/java/com/example/allaccountbook/uiPersistent/CustomDatePickerDialog.kt
@@ -0,0 +1,40 @@
+package com.example.allaccountbook.uiPersistent
+
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import java.text.SimpleDateFormat
+import java.util.*
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CustomDatePickerDialog(
+    showDialog: Boolean,
+    onDismiss: () -> Unit,
+    onDateSelected: (String) -> Unit
+) {
+    if (showDialog) {
+        val datePickerState = rememberDatePickerState()
+        DatePickerDialog(
+            onDismissRequest = onDismiss,
+            confirmButton = {
+                OutlinedButton(onClick = {
+                    datePickerState.selectedDateMillis?.let { millis ->
+                        val date = Date(millis)
+                        val format = SimpleDateFormat("yyyy년 M월", Locale.KOREA)
+                        onDateSelected(format.format(date))
+                    }
+                    onDismiss()
+                }) {
+                    Text("확인")
+                }
+            },
+            dismissButton = {
+                OutlinedButton(onClick = onDismiss) {
+                    Text("취소")
+                }
+            }
+        ) {
+            DatePicker(state = datePickerState)
+        }
+    }
+}
